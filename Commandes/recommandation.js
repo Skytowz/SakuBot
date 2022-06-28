1124b1ba3a944d27df9ad9a38852468861e54bc8
Skytowz <lucashottin359@gmail.com>
2022-06-28 11:09:00 +0200

    Ajout commande Reco

---
+++ Commandes/recommandation.js
@@ -0,0 +1,15 @@
+const { getNautiljonPageEmbed } = require("../services/nautiljonService");
+module.exports.run = async(client, message, args) =>{  
+    var urlR = /^(https?|chrome):\/\/[^\s$.?#].[^\s]*$/gm;
+    if(!args[1] || !args[1].match(urlR)) return message.channel.send("Il faut rentrer une URL Nautiljon");
+    const embed = await getNautiljonPageEmbed(message.author.username,args[1]);
+    if(typeof embed == "string") message.channel.send(embed);
+    else message.channel.send({embeds : [embed]});    
+};
+
+
+module.exports.help = {
+    name:"reco",
+    help:"> Recommande un manga/anime",
+    cmd:"reco <link-nautiljon>"
+}
--- utils/embed.js
+++ utils/embed.js
@@ -1,3 +1,5 @@
+const { cp } = require("fs");
+
 module.exports = class Embed{
 
     type = "rich"
@@ -65,4 +67,79 @@ module.exports = class Embed{
         return this;
     }
 
+
+    static getNautiljonEmbed(username,props,link){
+        const getDescription = (description) =>{
+            if(description.length > 500) return description.substr(0,500).trim()+`..........([see more](${link}))`;
+            else return description;
+        }
+        const otherData = [];
+        const datas = props.data.reduce((json, item) => {
+            if(item.includes("·")){
+                const elements = item.split("·");
+                if(elements){
+                    elements.forEach(element=>{
+                        if(element.includes(":")){
+                            const data = element.split(":");
+                            json[data.shift().trim()] = data.join(":").trim(); 
+                        }else{
+                            otherData.push(element);
+                        }
+                    })
+                }
+            }else{
+                if(item.includes(":")){
+                    const data = item.split(":");
+                    json[data.shift().trim()] = data.join(":").trim(); 
+                }
+            }
+            return json;
+        },{});
+        const embed =  new Embed()
+        .setTitle(props.infos[1])
+        .setThumbnail('https://www.nautiljon.com'+props.cover)
+        .addField("Name",`[${props.infos[2]}](${link})`);
+        if(datas.Auteur) embed.addField("Auteur",datas.Auteur);
+        if(datas.Origine && props.infos == "Mangas") embed.addField("Origine",datas.Origine);
+        if(otherData[0]){
+            embed.addField("Type",otherData[0]);
+            if(otherData[1]){
+                embed.addField("Pays d'origine",otherData[1])
+                if(otherData[2]){
+                    embed.addField("Durée",otherData[2]);
+                }
+            }
+        }
+        if(datas.Saison) embed.addField("Saison",datas.Saison);
+        if(datas.Type) embed.addField("Type",datas.Type);
+        if(datas.Genres) embed.addField("Genre",datas.Genres);
+        if(datas.Plateforme) embed.addField("Plateforme",datas.Plateforme);
+        if(datas.Prix) embed.addField("Prix",datas.Prix);
+        if(datas["Thème"]) embed.addField("Thème",datas["Thème"]);
+        if(datas["Thèmes"]) embed.addField("Thèmes",datas["Thèmes"]);
+        if(datas['Simulcast / streaming']) embed.addField("Disponible sur",datas['Simulcast / streaming']);
+        embed.addField(props.titre[0],getDescription(props.description[0]));
+        embed.setFooter("Recommandation par "+username);
+        switch(props.infos[1]){
+            case "Mangas": 
+                embed.setColor("BLACK");
+                break;
+            case "Animes":
+                embed.setColor("ORANGE");
+                break;
+            case "Light novels":
+                embed.setColor("BLUE");
+                break;
+            case "Jeux vidéo":
+                embed.setColor("RED");
+                break;
+            case "Personnalités":
+                embed.setColor("GREEN");
+                break;
+            default:
+                embed.setColor('DEFAULT');
+                break;
+        }
+        return embed;
+    }
 }
