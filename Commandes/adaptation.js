ae2506aeee1c1bdf79474e0a96eaa7dfa6092c14
Skytowz <lucashottin359@gmail.com>
2022-06-28 12:50:51 +0200

    adaptation

---
+++ Commandes/adaptation.js
@@ -0,0 +1,10 @@
+module.exports.run = async(client, message, args) =>{        
+     
+    await message.channel.send("https://docs.google.com/spreadsheets/d/1AmXILgVd1BFfOfxQn8OJrgMQCzWbBMBRQoD9o_OHTNw")
+
+};
+module.exports.help = {
+    name:"adaptation",
+    help:"> Envoie le fichier excel des chapitres adapté en anime",
+    cmd:"adaptation"
+}
