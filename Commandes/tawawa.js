86cf6de94c3c84d0f55b573c2a8d0665ee21a470
Skytowz <lucashottin359@gmail.com>
2022-06-30 15:27:34 +0200

    tawawachap

---
+++ Commandes/tawawa.js
@@ -0,0 +1,11 @@
+const { getChapitre } = require("../services/mangascanService");
+const { send } = require("../utils/mangaUtils");
+module.exports.run = async(client, message, args) =>{        
+    send(message,args,"6b27cbd8-4cc6-40ca-b010-928da4540be8");
+};
+module.exports.help = {
+    name:["tc","tawawachap","tchap"],
+    help:"> Affiche une page d'un chapitre de Tawawa",
+    cmd:"tc/tawawachap/tchap <chap> [page]",
+    commandeReste : true,
+}
