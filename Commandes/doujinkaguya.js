dbfb6f57e6ee9e8073af448d15cf6d43847572a9
Skytowz <lucashottin359@gmail.com>
2022-07-06 17:02:23 +0200

    Doujin Kaguya

---
+++ Commandes/doujinkaguya.js
@@ -0,0 +1,10 @@
+const { send } = require("../utils/mangaUtils");
+module.exports.run = async(client, message, args) =>{        
+    send(message,args,"a84264f6-a979-4573-89b3-09dd5c050c2b");
+};
+module.exports.help = {
+    name:["dkaguchap","dkc","dkchap"],
+    help:"> Affiche une page d'un chapitre de Kaguya-sama",
+    cmd:"dkaguchap/dkc/dkchap <chap> [page]",
+    commandeReste : true,
+}
