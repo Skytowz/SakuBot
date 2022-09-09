e4ad2f296a53475b106a26946fb8b95316172091
Skytowz <lucashottin359@gmail.com>
2022-09-10 01:06:57 +0200

    Revert "Revert "ajout de la commande CHAD""
    This reverts commit f5d9d6905083e3e6c6c95012d77a967ed299a957.

--- Commandes/chad.js
+++ Commandes/chad.js
@@ -1,13 +1,30 @@
-module.exports.run = async(client, message, args) =>{        
-     
-    if(message.type == "REPLY"){
-        const messageReference = await message.channel.messages.fetch(message.reference.messageId);
-        console.log(messageReference.embeds);
-    }
+const Canvas= require("@napi-rs/canvas");
+const { ApplicationCommandOptionType, CommandInteraction, AttachmentBuilder } = require("discord.js");
+const { request } = require("undici");
+const TypeHelp = require("../entity/typeHelp");
+const SlashOption = require("../utils/slashOption");
+
+module.exports.run = async(client, interaction) =>{  
+    const canvas = Canvas.createCanvas(678,761);
+    const context = canvas.getContext('2d');
+    
+    const background = await Canvas.loadImage("https://media.discordapp.net/attachments/991387297767510167/1017410101574914088/unknown.png?width=498&height=559");
+    context.drawImage(background,0,0,canvas.width,canvas.height);
+    
+    const user = interaction.options.getMentionable("mention") ?? interaction.user;
+    const { body } = await request(user.displayAvatarURL({extension:'jpg'}))
+    const avatar = await Canvas.loadImage(await body.arrayBuffer());
+    context.drawImage(avatar,150,60,avatar.width*3,avatar.height*3);
+    
+    const buffer = await canvas.encode('png');
+    interaction.reply({files:[buffer]});
 };
 module.exports.help = {
     name:"chad",
-    help:"> Envoie un photomontage de soit meme chad ou d'une personne tag en Chad",
+    help:"Envoie un photomontage de soit meme chad ou d'une personne tag en Chad",
     cmd:"chad [tag]",
-    noHelp:true
+    type:TypeHelp.Autre,
+    args:[
+        new SlashOption().setName("mention").setDescription("Le chad").setType(ApplicationCommandOptionType.Mentionable)
+    ]
 }
