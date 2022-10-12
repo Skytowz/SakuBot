b40dbfa5e64193c7b8d97f59ac1febd4f715b6f7
Skytowz <lucashottin359@gmail.com>
2022-10-12 21:10:35 +0200

    Ajout d'une image dans gogole, de la commande Miko et de la Commande Hayasaka ainsi que la possibilité d'envoyer du random depuis le answer

--- Commandes/answer.js
+++ Commandes/answer.js
@@ -2,7 +2,7 @@ const answers = require("./answer.json");
 
 module.exports.run = async(client, interaction) =>{       
     const answer = Object.values(answers).find(answer=>answer.name.includes(interaction.commandName));
-    interaction.reply(answer.send);
+    interaction.reply(answer.send.sample());
 };
 module.exports.help = {
     noHelp:true,
--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const client = new Client({ intents: [
 ]});
 
 Array.prototype.sample = function(){
+    if(this.length == 1) return this[0];
     return this[Math.floor(Math.random()*this.length)];
 }
 
