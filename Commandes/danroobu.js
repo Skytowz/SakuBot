b8b5d51d6e0b7c56923088659352803097c4ede0
Skytowz <lucashottin359@gmail.com>
2023-06-12 13:20:01 +0200

    Danroobu to Gelbooru + ajout Bokita + ajout solo

--- Commandes/danroobu.js
+++ Commandes/danroobu.js
@@ -1,16 +1,24 @@
 const danbooru = require("./danroobu.json");
 const { getGeneralImageByTag } = require("../services/danroobuService");
 const superagent = require("superagent");
+const { ApplicationCommandOptionType } = require("discord.js");
+const SlashOption = require("../utils/slashOption");
 
 
 module.exports.run = async(client, interaction) =>{      
     const name = Object.values(danbooru).find(cmd =>cmd.name.includes(interaction.commandName));
-
-    const url = await getGeneralImageByTag(name.research); 
-    interaction.reply(url);
+    const url = await getGeneralImageByTag(name.research,interaction.options.getBoolean("solo")); 
+    if(url){
+        interaction.reply(url);
+    }else{
+        interaction.reply({content:"Aucune image trouvé",ephemeral:true})
+    }
 
 };
 module.exports.help = {
     noHelp:true,
-    slash:true
+    slash:true,
+    args:[
+        new SlashOption("solo","Solo",ApplicationCommandOptionType.Boolean)
+    ]
 }
