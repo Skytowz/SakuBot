6915b0b64a853fab5be981ec7879181d30331519
Skytowz <lucashottin359@gmail.com>
2023-06-15 23:25:18 +0200

    On aime pas les enfants

--- services/danroobuService.js
+++ services/danroobuService.js
@@ -1,3 +1,3 @@
 const superagent = require("superagent");
 
-module.exports.getGeneralImageByTag = async (name,solo,sensitive) => await superagent.get(`https://gelbooru.com/index.php?tags=${ name }${solo ? "+solo" : ""}+sort:random+-video+rating:${sensitive ? "sensitive" : "general"}&page=dapi&json=1&s=post&q=index&limit=1`).set("User-Agent","Discord Bot Hayasaku").then(res => res?.body?.post?.pop()?.file_url);
+module.exports.getGeneralImageByTag = async (name,solo,sensitive) => await superagent.get(`https://gelbooru.com/index.php?tags=${ name }${solo ? "+solo" : ""}+sort:random+-video+rating:${sensitive ? "sensitive+-loli+-child+-lolita+-lolicon+-shota" : "general"}&page=dapi&json=1&s=post&q=index&limit=1`).set("User-Agent","Discord Bot Hayasaku").then(res => res?.body?.post?.pop()?.file_url);
