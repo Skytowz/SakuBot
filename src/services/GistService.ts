8b6460f3db92f3ea4cc54681d12e3e6180bce90a
Skytowz <lucashottin359@gmail.com>
2023-12-11 21:46:24 +0100

    gnc back proxy imgurimpl

--- src/services/GistService.ts
+++ src/services/GistService.ts
@@ -4,6 +4,9 @@ import AbstractService, { SERVICE_BEAN_TYPE } from './AbstractService.js';
 import injector, { Bean } from 'wire-dependency-injection';
 
 export default class GistService extends AbstractService {
+
+  private CLIENT_ID : string = "Client-ID b7c69d2903407d9";
+
   public constructor(bean: Bean) {
     super(bean.getId());
   }
@@ -20,11 +23,12 @@ export default class GistService extends AbstractService {
   }
 
   public async getChapitreById(
-    chapitre: { title: string; groups: Array<Array<string>> },
+    chapitre: { title: string; groups: Array<Array<string>> | object },
     number: number | string,
     cubariId: string
   ) {
-    const pages = Object.values(chapitre.groups).pop();
+    let pages = Object.values(chapitre.groups).pop();
+    if(typeof pages === 'string') pages = await this.getPagesImgur(pages);
     return new Chapitre(
       pages as Array<string>,
       number as string,
@@ -34,6 +38,21 @@ export default class GistService extends AbstractService {
       `https://cubari.moe/read/gist/${cubariId}/${number}`,
       []
     );
+  }
+
+  public async getPagesImgur(proxy : string){
+    const idImgur : string | undefined = proxy.match(/\/([^\/]*)$/)?.at(1);
+    if(idImgur === undefined) throw new Error("Erreur au niveau du chapitre");
+    return await fetch(
+      `https://api.imgur.com/3/album/${idImgur}/images`,
+      {
+        method:'get',
+        headers:{
+          "Authorization":this.CLIENT_ID
+        }
+      })
+    .then(res => res.json())
+    .then(res => res.data.map((e : any)=>e.link));
   }
 }
 
