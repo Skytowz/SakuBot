95c3d76ae560dcb54be3d772d8e7dafd43fd1806
SeanR-ScanR <scanr.seanr@gmail.com>
2024-05-28 22:19:09 +0200

    Update Rule Auto Kick

--- src/handlers/commandesMessageHandler/DiscordBotSpamHandler.ts
+++ src/handlers/commandesMessageHandler/DiscordBotSpamHandler.ts
@@ -23,7 +23,7 @@ export default class DiscordBotSpamHandler extends AbstractMessageHandler {
   public constructor() {
     super({
       id: 'DiscordBotSpam',
-      regex: /(?=.*discord\.gg\/[^\s]+)(?=.*@everyone)/,
+      regex: /(?=.*http)(?=.*@everyone)/,
     });
   }
 
